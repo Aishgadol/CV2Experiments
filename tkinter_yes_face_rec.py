08496794208986c97b0216375ba7091c3937d5bb
Aishgadol <xanjera@gmail.com>
2023-10-01 01:44:16 +0300

    update

--- tkinter_yes_face_rec.py
+++ tkinter_yes_face_rec.py
@@ -5,73 +5,80 @@ import random
 from PIL import Image, ImageTk
 import face_recognition
 
+colors = [cv2.COLOR_BGR2YCrCb, cv2.COLOR_BGR2GRAY, cv2.COLOR_BGR2HLS, cv2.COLOR_BGR2HSV, cv2.COLOR_BGR2LAB,
+          cv2.COLOR_BGR2XYZ]
 
-colors=[cv2.COLOR_BGR2YCrCb, cv2.COLOR_BGR2GRAY,cv2.COLOR_BGR2HLS,cv2.COLOR_BGR2HSV,cv2.COLOR_BGR2LAB,cv2.COLOR_BGR2XYZ]
-# Create a function to update the video feed
+
+# function to update the video feed
 def update_video_feed():
-    # Read a frame from the camera
-
+    # read the current frame from live feed camera
     ret, frame = cap.read()
     if ret:
-        face_locations=face_recognition.face_locations(frame)
+        # get all locations of faces in frame
+        face_locations = face_recognition.face_locations(frame)
+        # draw a square around each face
         for face_loc in face_locations:
             rec_colors = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-            cv2.rectangle(frame,(face_loc[3],face_loc[0]),(face_loc[1],face_loc[2]),color=rec_colors,thickness=2)
-        #if suprise is pressed than color will change
+            cv2.rectangle(frame, (face_loc[3], face_loc[0]), (face_loc[1], face_loc[2]), color=rec_colors, thickness=2)
+        # if suprise is pressed than color will randomly change
         if not show_surprise:
-            frame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         else:
-            frame=cv2.cvtColor(frame,colors[num])
+            frame = cv2.cvtColor(frame, colors[num])
 
-        # Convert the frame to a format that Tkinter can display
+        # used gpt for this, need to convert frame from cv2 format to tkinter format so it can be displayed
         photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
         label.config(image=photo)
         label.image = photo
 
-    # Schedule the function to run again after 10 milliseconds
+    # update video feed after 5 miliseconds, call the function on the ttk window object and not in a 'normal' call
     window.after(5, update_video_feed)
 
-# Function to toggle the "surprise" text
+
+# function to change the button text, as well as generate random number to pick a color to display(random color changer)
 def toggle_surprise():
-    if toggle_button['text']=="Surprise is Off":
-        toggle_button['text']="Surprise is On"
+    if toggle_button['text'] == "Surprise is Off":
+        toggle_button['text'] = "Surprise is On"
     else:
         toggle_button['text'] = "Surprise is Off"
     global show_surprise
     show_surprise = not show_surprise
     global num
-    num = random.randint(0, len(colors)-1)
+    # by changing it here, every press of the button will generate random num, and thus livefeed won't constantly
+    # change colors but just once, everytime we press the button
+    num = random.randint(0, len(colors) - 1)
 
-# Initialize OpenCV video capture
+
+# initialize opencv video capture
 cap = cv2.VideoCapture(0)
 
-# Create a Tkinter window
+# create the window where everything will be shown
 window = tk.Tk()
 window.title("Video Feed")
 
-# Create a label to display the video feed
+# this label will hold the video feed
 label = ttk.Label(window)
 label.pack(padx=10, pady=10)
 
-# Create a button to toggle the "surprise" text
+# create the surprise button
 toggle_button = ttk.Button(window, text="Surprise is Off", command=toggle_surprise)
 toggle_button.pack(pady=10)
 global rec_colors
 
-# Initialize the "show_surprise" variable
+# initialize global variables
 show_surprise = False
 num = 0
-rec_colors=(0,0,0)
-# Start updating the video feed
+rec_colors = (0, 0, 0)
+# updating video feed with rgb colors or random colors according to show_suprise
 update_video_feed()
 
-# Create a quit button to exit the application
+# quit button which will never be used because we close it using the x button
 quit_button = ttk.Button(window, text="Quit", command=window.quit)
 quit_button.pack(pady=10)
 
-# Run the Tkinter main loop
+# run the tkinter mainloop after all parameters have been set
 window.mainloop()
 
-# Release the camera when the application is closed
+# gpt helped adding the cap.release() line, appearntly we need to release the camera before closing the window and not after
 cap.release()
 cv2.destroyAllWindows()
